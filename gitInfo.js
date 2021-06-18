ca4f0d0a3c915c78cf4aa70ea1d05d0c593e8a43
mroseweir <roseweirmichael@gmail.com>
2021-06-18 11:31:50 -0500

    adds final answers

---
+++ gitInfo.js
@@ -0,0 +1,95 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+let gitDefinition = 'Git is a version control software.'
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+let gitHubDefinition = 'Github is a online portal for Git, allowing you to upload work to a repository'
+
+//////////////////PROBLEMS 3 - 9////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+let init = {
+    description: 'creates a new repository',
+    code: 'git init'
+}
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+let clone = {
+    description: 'creates a clone/copy of an existing repo',
+    code: 'git clone'
+}
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+let status = {
+    description: 'Lets you know if you have made any modifications to the file',
+    code: 'git status',
+}
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+let add = {
+    description: 'Adds a change that you have made, changes wont be made to github till you commit',
+    code: 'git add',
+}
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+let commit = {
+    description: 'Saves all the changes you have made to your local repo',
+    code: 'git commit',
+}
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+let push = {
+    description: 'Upload the current local repo to the remote github repo',
+    code: 'git push',
+}
